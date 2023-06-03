e354aaa475d6f26d347d74bff0b08c239d766302
Nicholas Galante <nicholasgalante94@gmail.com>
2023-06-03 13:54:31 -0400

    commit tree 88a815c256b14e0ff7ef6738d051b8fc975fd23f

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -8,7 +8,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant_by_id
     render json: plant
   end
 
@@ -17,10 +17,42 @@ class PlantsController < ApplicationController
     plant = Plant.create(plant_params)
     render json: plant, status: :created
   end
+  
+  # PATCH /plants/:id
+  def update
+    plant = find_plant_by_id
+    if plant
+      plant.update(plant_params)
+      render json: plant, status: :ok
+    else 
+      plant_not_found
+    end
+  end
+
+
+  # DELETE /plants/:id
+  def destroy
+    plant = find_plant_by_id
+    if plant
+      plant.destroy  
+      render json: {}, status: :no_content
+    else 
+      plant_not_found    
+    end
+  end
+
 
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def find_plant_by_id
+    Plant.find_by(id: params[:id])
+  end
+
+  def plant_not_found
+    render json: { error: "Plant not found" }, status: :not_found
   end
 end
